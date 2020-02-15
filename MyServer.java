61648adbb52b1a253e2d2bf0173c27022c780a34
Sreejith S <sreejithsankar55@gmail.com>
2020-02-15 23:03:47 +0530

    Added hostip

--- MyServer.java
+++ MyServer.java
@@ -5,7 +5,7 @@ public class MyServer{
 
 public static void main(String args[]){
 try{
-
+System.setProperty("java.rmi.server.hostname","10.113.8.115");
 Distributed stub=new DistributedRemote();
 Naming.rebind("rmi://localhost:5000/run",stub);
 
