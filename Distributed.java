a780ccf7d4bcea31ee0c8b7557a833f95ad2c778
gopavasanth <gopavasanth1999@gmail.com>
2020-02-16 00:43:14 +0530

    - Removed content file. - Removed hardcoding of ip-address of users.

--- Distributed.java
+++ Distributed.java
@@ -1,8 +1,8 @@
 import java.rmi.*;
 public interface Distributed extends Remote{
 
-public void writing(String input) throws RemoteException;
-public void reading(int i) throws RemoteException;
-public void lock() throws RemoteException;
-public void release() throws RemoteException;
+    public void writing(String input) throws RemoteException;
+    public void reading(int i) throws RemoteException;
+    public void lock() throws RemoteException;
+    public void release() throws RemoteException;
 }
--- Reader1.java
+++ Reader1.java
@@ -2,13 +2,13 @@ import java.rmi.*;
 
 public class Reader1{
 
-public static void main(String args[]){
-try{
-
-Distributed stub=(Distributed)Naming.lookup("rmi://localhost:5000/run");
-stub.reading(1);
-
-}catch(Exception e){System.out.println(e);}
-}
+    public static void main(String args[]){
+        try {
+            Distributed stub=(Distributed)Naming.lookup("rmi://localhost:5000/run");
+            stub.reading(1);
+        } catch(Exception e){
+            System.out.println(e);
+        }
+    }
 
 }
